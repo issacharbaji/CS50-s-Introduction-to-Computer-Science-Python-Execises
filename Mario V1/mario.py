d6c6b198d359c8354bfbf1ee5c9c9aa0ab48e931
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-05-31 10:18:42 +0300

    Create mario.py

---
+++ Mario V1/mario.py
@@ -0,0 +1,26 @@
+from cs50 import get_int
+
+
+def main():
+    height = get_int("Height :")
+    while True:
+        if height >= 1 and height <= 8:
+            trace(height)
+            exit(0)
+        else:
+            main()
+
+
+def trace(height):
+    for i in range(1, height + 1):
+        for j in range(1, height + 1):
+            if j <= height - i:
+                print(" ", end="")
+            else:
+                print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    main()
+
